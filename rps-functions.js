cc61031589e5b7de0e30a577f81c690368cb549d
Joel <joel.knoll1@gmail.com>
2022-08-26 23:56:15 -0400

    Streamline loop structure. Add timeout to end-game alerts.

--- rps-functions.js
+++ rps-functions.js
@@ -5,13 +5,19 @@ function playRound(playerSelection){
 
     if (roundWinner === `cpu`){
         cpuScore += 1;
-        alert(`You played ${playerSelection}. Computer played ${cpuSelection}. Computer wins.`);
         cpuScoreDisplay.innerHTML = `${cpuScore}`;
+        alert(`You played ${playerSelection}. Computer played ${cpuSelection}. Computer wins.`);
+        setTimeout(() => {
+            endGame();
+        }, 1);
     } 
     else if (roundWinner === `player`){
         playerScore += 1;
-        alert(`You played ${playerSelection}. Computer played ${cpuSelection}. You win.`);
         playerScoreDisplay.innerHTML = `${playerScore}`;
+        alert(`You played ${playerSelection}. Computer played ${cpuSelection}. You win.`);
+        setTimeout(() => {
+            endGame();
+        }, 1);
     }
     else if (roundWinner === `draw`){
         alert(`You played ${playerSelection}. Computer played ${cpuSelection}. This round's a draw!`);
@@ -61,6 +67,42 @@ function determineRoundWinner(cpuSelection, playerSelection){
     return winner; 
 }
 
+function endGame(){
+ 
+    if (playerScore === 5){
+        
+        alert("Player wins!");
+        
+        let playAgain = prompt("Would you like to play again? [y/n]");
+        
+        if (playAgain === "y"){
+            playerScore = 0;
+            cpuScore = 0;
+            cpuScoreDisplay.innerHTML = `${cpuScore}`;
+            playerScoreDisplay.innerHTML = `${playerScore}`;
+        } else{
+            alert('Thanks for playing!'); 
+        }
+    }
+    
+    if (cpuScore === 5){
+        
+        alert("Computer wins!");
+        
+        let playAgain = prompt("Would you like to play again? [y/n]");
+        
+        if (playAgain === "y"){
+            playerScore = 0;
+            cpuScore = 0;
+            cpuScoreDisplay.innerHTML = `${cpuScore}`;
+            playerScoreDisplay.innerHTML = `${playerScore}`;
+        } else{
+            alert('Thanks for playing!'); 
+        }
+    }
+
+}
+
 let cpuScore = 0;
 let playerScore = 0;
 
@@ -72,5 +114,9 @@ buttons.forEach(addEventListener("click", (e) =>
     playRound(e.target.innerHTML.toLowerCase())));
 
 //PROBLEM: How to end the game once either player or computer has scored 5 points?
+//Options: When either playerScore or cpuScore reaches 5:
+//-disable the button
+//-have button tell player games is over and give option of playing again. If yes, restore
+//appropriate event listener 
 
 
